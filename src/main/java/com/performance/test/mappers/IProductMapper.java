1ba2a5d4f6643e25a9bea9a0cf037dcb4f4eb075
SantiPelaez07 <santipelaez0@gmail.com>
2024-07-29 10:34:00 -0500

    Creation of interfaces for mappers

---
+++ src/main/java/com/performance/test/mappers/IProductMapper.java
@@ -0,0 +1,10 @@
+package com.performance.test.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface IProductMapper {
+
+    
+}
